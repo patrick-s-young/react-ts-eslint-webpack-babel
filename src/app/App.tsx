e296eea9677f0482e7600629bd9ba8df7d48b135
Patrick S. Young <patrick@bigeyeball.co>
2020-10-22 11:42:06 -0700

    add redux toolkit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Input from 'components/input/Input';
+import { useSelector } from 'react-redux';
+import type { RootState } from 'app/rootReducer';
+import type { AppState } from 'app/appSlice';
 import 'app/app.css';
 
 const App: React.FC = () => {
+	const { greeting } = useSelector<RootState, AppState>((state) => state.app as AppState);
 	return(
 			<div className='hello-flex'>
 				<div>
-					<h1>Hello World</h1>
+					<h1>{greeting}</h1>
 				</div>
 				<div>
 					<Input id="hello-input" label="Hello Input" />
